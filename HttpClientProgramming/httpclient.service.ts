89fabeb65a468d91de9125077e101514ed986eff
arvinnon <83747422+arvinnon@users.noreply.github.com>
2022-02-27 01:49:55 +0800

    Add files via upload

---
+++ HttpClientProgramming/httpclient.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpclientService {
+  constructor(private http:HttpClient) { }
+  getUsersfromServer () {
+    return this.http.get('https://jsonplaceholder.typicode.com/users')
+  }
+  getOtherUsersfromServer () {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=5%27')
+  }
+}
